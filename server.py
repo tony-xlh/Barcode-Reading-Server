69c96c954aefe999ff38e89dd0e861845310d957
xulihang <xulihanghai@163.com>
2023-05-06 10:48:39 +0800

    use http

--- server.py
+++ server.py
@@ -43,4 +43,4 @@ def read_barcodes():
         return ""
 
 if __name__ == '__main__':
-   app.run(host = "0.0.0.0", port = 8888, ssl_context='adhoc')
+   app.run(host = "0.0.0.0", port = 8888) #, ssl_context='adhoc'
